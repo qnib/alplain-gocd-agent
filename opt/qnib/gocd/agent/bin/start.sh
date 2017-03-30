8da939886690e7a040510918d16100d51b4b2ae2
Christian Kniep <ckniep@gaikai.com>
2017-03-30 12:22:38 +0200

    adjust agent startup

--- opt/qnib/gocd/agent/bin/start.sh
+++ opt/qnib/gocd/agent/bin/start.sh
@@ -1,20 +1,10 @@
-#!/usr/local/bin/dumb-init /bin/bash
+#!/bin/bash
 
-function extend_list {
-    python -c "import sys ; first = set(sys.argv[1].split(',')) ; all = first.union(set(sys.argv[2:])) ; print ','.join(all)" $@
-}
 
-if [ "X${GOCD_SKIP_WAIT}" != "Xtrue" ];then
-    source /opt/qnib/consul/etc/bash_functions.sh
-    wait_for_srv gocd-server
-fi
+#if [ "X${GOCD_LOCAL_DOCKERENGINE}" == "Xtrue" ];then
+#	GOCD_AGENT_AUTOENABLE_RESOURCES=$(extend_list ${GOCD_AGENT_AUTOENABLE_RESOURCES} docker-engine)
+#fi
 
-if [ "X${GOCD_LOCAL_DOCKERENGINE}" == "Xtrue" ];then
-	GOCD_AGENT_AUTOENABLE_RESOURCES=$(extend_list ${GOCD_AGENT_AUTOENABLE_RESOURCES} docker-engine)
-fi
+#consul-template -once -template "/etc/consul-templates/gocd/autoregister.properties.ctmpl:/opt/go-agent/config/autoregister.properties"
 
-consul-template -once -template "/etc/consul-templates/gocd/autoregister.properties.ctmpl:/opt/go-agent/config/autoregister.properties"
-
-/opt/go-agent/agent.sh 2>&1 1>/var/log/gocd-agent.log &
-echo $$ > /opt/go-agent/go-agent.pid
-tail -f /var/log/gocd-agent.log
+/opt/go-agent/agent.sh
