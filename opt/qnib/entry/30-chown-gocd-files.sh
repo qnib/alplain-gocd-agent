dccc8dfabf7409eb1847eea3acf02d31467253f4
Christian Kniep <christian@qnib.org>
2022-03-24 18:04:46 +0100

    update base

--- opt/qnib/entry/30-chown-gocd-files.sh
+++ opt/qnib/entry/30-chown-gocd-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chown -R gocd /opt/go-agent/
+chown -R go /opt/go-agent/
 if [[ -e /var/run/docker.sock ]];then
-   chown gocd /var/run/docker.sock
+   chown go /var/run/docker.sock
 fi
